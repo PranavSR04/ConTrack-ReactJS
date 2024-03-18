95e361b288e2abecae8635f4ef976c06d1f52c65
Dantus George Tom <dantus.tom@experionglobal.local>
2024-03-18 08:48:20 +0530

    fix:dashboard update

--- src/Components/RevenueScatterPlot/ScatterPlotHandler.tsx
+++ src/Components/RevenueScatterPlot/ScatterPlotHandler.tsx
@@ -43,7 +43,7 @@ const ScatterPlotHandler = () => {
                     pointHoverBackgroundColor: 'rgba(75,192,192,1)',
                     pointHoverBorderColor: 'rgba(220,220,220,1)',
                     pointHoverBorderWidth: 2,
-                    pointRadius: 5,
+                    pointRadius: 3,
                     pointHitRadius: 8,
             }
         ]
