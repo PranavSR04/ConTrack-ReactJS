c2f8b8d0f8d8d0d77775458e91d3c8e617a73480
Dantus George Tom <dantus.tom@experionglobal.local>
2024-03-18 16:25:41 +0530

    fix:revenueprojection and dashbboard

--- src/Components/DashBoardRevenue/DashBoardRevenue.tsx
+++ src/Components/DashBoardRevenue/DashBoardRevenue.tsx
@@ -24,10 +24,10 @@ console.log({previousMonthRevenue})
   return (
     <Row gutter={16}>
     <Col span={12}>
-      <Card style={{width:'135px',height:'120px',padding:'0px',transform:'scale', backgroundColor:'#f4f4f4', border:'solid 2px #f9f9f9'}}>
-      <p style={{fontSize:'15px', paddingLeft:'13%', marginBottom:'0.1rem'}}>{responsetype}</p> 
+      <Card style={{width:'135px',height:'120px',padding:'0px',transform:'scale', backgroundColor:'#f4f4f4', border:'solid 2px #f9f9f9',}}>
+      <p style={{fontSize:'15px', paddingLeft:'2%', marginBottom:'0.1rem'}}>{responsetype}</p> 
       {/* <CountUp end={currentMonthRevenue} style={{fontSize:'20px', fontWeight:'600', marginLeft:'10.8px'}}/> */}
-      <div style={{fontSize:'15px', fontWeight:'600', marginLeft:'10.8px'}}>
+      <div style={{fontSize:'15px', fontWeight:'600', marginLeft:'1.8px'}}>
       <span>USD&nbsp;</span>
       <CountUp end={currentMonthRevenue/1000.00}/>
       <span>k&nbsp;</span>
@@ -38,7 +38,7 @@ console.log({previousMonthRevenue})
           valueStyle={{ color: difference >= 0 ? '#3f8600' : 'red' }}
           prefix={arrowIcon}
           suffix="%"
-          style={{marginLeft:'0.9rem', transform:'scale(1.2)', marginTop:'0.2rem'}}
+          style={{marginLeft:'0.5rem', transform:'scale(1.2)', marginTop:'0.2rem'}}
         />
       </Card>
       
